c769329f0e93dc38ebf11ab0c192bf8e93f7b952
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-01-25 21:42:49 -0600

    Talked about C# basics

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Spring 2021
+ * Sample Implementation
+ */
+using System;
 
 namespace HelloWorld
 {
+    //Single line comment
+    //Single line comment again
+
+    /*
+     * Multi line comment
+     */
+
     class Program
     {
-        static void Main(string[] args)
+        //Function in type 
+        // [modifiers] return-based identifier ( [parameters] ) { }
+        //     paramaters ::= parameter { , parameter }*
+        //     parameter ::= T identifier
+        static void Main ( string [] args )    //C++ only ( void )
         {
+            //Outputs to console window 
             Console.WriteLine("Hello World!");
         }
     }
