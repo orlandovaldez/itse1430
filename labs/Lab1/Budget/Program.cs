3d5892f08014eb6170f7cee107fce774d4b207b1
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-01-31 00:43:36 -0600

    Added Account Name Input Validation

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -17,6 +17,15 @@ namespace Budget
             Console.WriteLine("ITSE 1430");
             Console.WriteLine("Semester 2021");
             Console.WriteLine("Orlando Valdez");
+
+            Console.WriteLine("Account Name: ");
+            String accountName = Console.ReadLine();
+
+            while (String.IsNullOrEmpty(accountName))
+            {
+                Console.WriteLine("Value is required");
+                accountName = Console.ReadLine();
+            }
         }
     }
 }
