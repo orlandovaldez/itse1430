31114f3c23722fd71fe9d305360e369705cbebc6
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-01-30 23:27:55 -0600

    Added Header

---
+++ labs/Lab1/Budget/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Budget
+ * ITSE 1430
+ * Semester 2021
+ * Orlando Valdez
+ */
+
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
