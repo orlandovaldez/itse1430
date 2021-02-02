fe37dfda79e0f61a04c921489eae7d10d29fb1ce
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-02-02 03:51:28 -0600

    Redid Prompting For Account Info

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -14,8 +14,25 @@ namespace Budget
         static void Main(string[] args)
         {
             BasicProgramInfo();
+            PromptAccountInfo();
 
             
+        }
+
+        static string accountName;
+        static string accountNumber;
+        static string startingBalance;
+
+        private static void PromptAccountInfo ()
+        {
+            Console.Write("Enter Account Name:  ");
+            accountName = Console.ReadLine();
+
+            Console.Write("Enter Account Number:    ");
+            accountNumber = Console.ReadLine();
+
+            Console.Write("Enter Starting Balance:  ");
+            startingBalance = Console.ReadLine();            
         }
 
         private static void BasicProgramInfo ()
@@ -24,6 +41,11 @@ namespace Budget
             Console.WriteLine("ITSE 1430");
             Console.WriteLine("Semester 2021");
             Console.WriteLine("Orlando Valdez");
+        }
+
+        private static void DisplayError ( string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
