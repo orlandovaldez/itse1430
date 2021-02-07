46d19193115ed9fa8dbe097fedc401937fa71cf5
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-02-07 01:24:09 -0600

    Added Menu Selection

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -16,23 +16,92 @@ namespace Budget
             BasicProgramInfo();
             PromptAccountInfo();
 
+            bool done = false;
+            do
+            {
+                AccountInfo();
+                char mainMenuOption = DisplayMainMenu();
+
+                switch(mainMenuOption)
+                {
+                    case 'Q': done = true; break;
+
+                    default: DisplayError("Invalid Selection, Try Again"); break;
+                }
+            } while (!done);
             
+        }
+
+        private static bool QuitProgram ( bool v )
+        {
+            do
+            {
+                Console.WriteLine("Are You Sure You Want To Exit? Y/N: ");
+                string input = Console.ReadLine();
+
+                if (input == "Y" || input == "y")
+                    return true;
+                else if (input == "N" || input == "n")
+                    return false;
+                else
+                    DisplayError("Invalid Input, Select Y/N ");
+                
+            } while (true);
+        }
+
+        private static char DisplayMainMenu ()
+        {
+            
+            do
+            {
+                Console.WriteLine("A) Deposit");
+                Console.WriteLine("B) Withdraw");
+                Console.WriteLine("Q) Quit");
+
+                string menuInput = Console.ReadLine();
+
+                switch(menuInput)
+                {
+                    case "A":
+                    case "a": return 'A';
+
+                    case "B":
+                    case "b": return 'B';
+
+                    case "Q":
+                    case "q": return 'Q';
+                };
+
+                DisplayError("Invalid Selection, Try Again");
+            } while (true);
         }
 
         static string accountName;
         static string accountNumber;
-        static decimal startingBalance;
+        static decimal accountBalance;
 
+        private static void AccountInfo()
+        {
+            Console.WriteLine("______________________________");
+            Console.WriteLine($"Account Name: {accountName}");
+            Console.WriteLine($"Account Number: {accountNumber}");
+            Console.WriteLine($"Account Balance: ${accountBalance}");
+        }
+       
         private static void PromptAccountInfo ()
         {
-            Console.Write("Enter Account Name:  ");
+            Console.Write("Enter Account Name:   ");
             accountName = AccountNameCheck();
 
-            Console.Write("Enter Account Number:    ");
-            accountNumber = AccountNumberValid();
+            Console.Write("Enter Account Number: ");
+            accountNumber = Console.ReadLine();
+            //TODO: AccountNumberValid();
+            //      -Must consist of only digits (0-9).
+            //      -Must be exactly 12 characters long.
+            //      -May not start or end with a zero.  
 
             Console.Write("Enter Starting Balance:  ");
-            startingBalance = BalanceCheck(1);            
+            accountBalance = BalanceCheck(1);            
         }
 
         private static string AccountNumberValid ()
@@ -64,14 +133,14 @@ namespace Budget
             }return accountName;
         }
 
-        static decimal BalanceCheck(decimal minimumBalance )
+        private static decimal BalanceCheck(decimal minimumBalance )
         {
             do
             {
                 //Check to see if the starting balance of account is greater than zero 
-                string startingBalance = Console.ReadLine();
+                string accountBalance = Console.ReadLine();
 
-                if (Decimal.TryParse(startingBalance, out decimal result))
+                if (Decimal.TryParse(accountBalance, out decimal result))
                 {
                     if (result >= minimumBalance)
                         return result;
