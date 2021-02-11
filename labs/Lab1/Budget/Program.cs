d25305be02e6105a9f080d74adb49983dfa788d3
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-02-11 03:23:19 -0600

    Added To Allow Money Deposits and Deposit Description

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -11,7 +11,7 @@ namespace Budget
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
             BasicProgramInfo();
             PromptAccountInfo();
@@ -22,22 +22,80 @@ namespace Budget
                 AccountInfo();
                 char mainMenuOption = DisplayMainMenu();
 
-                switch(mainMenuOption)
+                switch (mainMenuOption)
                 {
-                    case 'Q': done = true; break;
+                    case 'A':
+                    Deposit(0); DepositDescribe();
+                    break;
+                    case 'B':
+                    break;
+                    case 'Q':
+                    break;
 
                     default: DisplayError("Invalid Selection, Try Again"); break;
                 }
             } while (!done);
-            
+
         }
 
-        private static bool QuitProgram ( bool v )
+        private static string DepositDescribe ()
+        {
+            Console.Write("Enter Deposit Description: ");
+            
+            depositDescribe = Console.ReadLine();
+            while (String.IsNullOrEmpty(depositDescribe))
+            {
+                Console.WriteLine("Descpription Required");
+                depositDescribe = Console.ReadLine();
+                Console.WriteLine("Balance Has Been Updated.");
+            }
+            return depositDescribe;
+
+            
+
+        }
+
+        private static decimal Deposit ( decimal minimumDeposit )
         {
             do
             {
-                Console.WriteLine("Are You Sure You Want To Exit? Y/N: ");
-                string input = Console.ReadLine();
+                Console.Write("Enter Deposit Amount: ");
+                string depositAmount = Console.ReadLine();
+
+                if (Decimal.TryParse(depositAmount, out decimal result))
+                {
+                    if (result >= minimumDeposit)
+                        return accountBalance = accountBalance + result;                 
+                    else
+                        DisplayError("Deposit Must Be Greater Than 0");
+                } else
+                    DisplayError("Value Must Be Numeric.");
+            } while (true);
+
+
+            
+
+            //TODO
+            //Provide info for deposit 
+            // Amount (As a positive number)
+            // Description (The Description of the transaction)
+
+            //Extra: Category, Check Number, Date(MM/dd/yyyy)
+        }
+
+        static string accountName;
+        static string accountNumber;
+        static decimal accountBalance;
+        static decimal depositAmount;
+        static string depositDescribe;
+
+
+        static bool QuitProgram ()
+        {
+            Console.WriteLine("Are You Sure You Want To Exit? Y/N: ");
+            string input = Console.ReadLine();
+            do
+            {
 
                 if (input == "Y" || input == "y")
                     return true;
@@ -45,13 +103,13 @@ namespace Budget
                     return false;
                 else
                     DisplayError("Invalid Input, Select Y/N ");
-                
+
             } while (true);
         }
 
         private static char DisplayMainMenu ()
         {
-            
+
             do
             {
                 Console.WriteLine("A) Deposit");
@@ -60,7 +118,7 @@ namespace Budget
 
                 string menuInput = Console.ReadLine();
 
-                switch(menuInput)
+                switch (menuInput)
                 {
                     case "A":
                     case "a": return 'A';
@@ -76,50 +134,55 @@ namespace Budget
             } while (true);
         }
 
-        static string accountName;
-        static string accountNumber;
-        static decimal accountBalance;
 
-        private static void AccountInfo()
+        private static void AccountInfo ()
         {
             Console.WriteLine("______________________________");
             Console.WriteLine($"Account Name: {accountName}");
             Console.WriteLine($"Account Number: {accountNumber}");
-            Console.WriteLine($"Account Balance: ${accountBalance}");
+            Console.WriteLine($"Account Balance: {accountBalance:C}");
         }
-       
+
         private static void PromptAccountInfo ()
         {
             Console.Write("Enter Account Name:   ");
             accountName = AccountNameCheck();
 
             Console.Write("Enter Account Number: ");
-            accountNumber = Console.ReadLine();
+            accountNumber = AccountNumberValid();
             //TODO: AccountNumberValid();
             //      -Must consist of only digits (0-9).
             //      -Must be exactly 12 characters long.
             //      -May not start or end with a zero.  
 
             Console.Write("Enter Starting Balance:  ");
-            accountBalance = BalanceCheck(1);            
+            accountBalance = BalanceCheck(1);
         }
 
         private static string AccountNumberValid ()
         {
-            do
-            {
-                string input = Console.ReadLine();
+            bool validNumber = false;
+            string input = Console.ReadLine();
+
+            
                 for (int index = 0; index < input.Length; ++index)
                 {
-                    if (!Char.IsDigit(input, index)) ;
-                    
+                    if (!Char.IsDigit(input, index))
+                    {
+
+                        DisplayError("Invalid Account Number");
+                        validNumber = false;
+                    } else
+                        Console.WriteLine("Number Accepted");
+                        validNumber = true;
+
+                    //break;
                 }
-                DisplayError("Number must consist of only digits");
+                return input;
 
 
+           
 
-
-            } while (true);
         }
 
         private static string AccountNameCheck ()
@@ -129,11 +192,12 @@ namespace Budget
             {
                 Console.WriteLine("Account Name Must Not Be Empty, Enter Valid Name");
                 accountName = Console.ReadLine();
-                
-            }return accountName;
+
+            }
+            return accountName;
         }
 
-        private static decimal BalanceCheck(decimal minimumBalance )
+        private static decimal BalanceCheck ( decimal minimumBalance )
         {
             do
             {
@@ -143,19 +207,17 @@ namespace Budget
                 if (Decimal.TryParse(accountBalance, out decimal result))
                 {
                     if (result >= minimumBalance)
+                    {
+                        Console.WriteLine("Valid Balance Accepted.");
                         return result;
-                    else
+                    } else
                         DisplayError("Balance Must Be Greater Than 0");
                 } else
-                    DisplayError("Value Must Be Numeric.");
+                    DisplayError("Value Must Be Numeric Or Not Empty.");
 
             } while (true);
         }
 
-        static void CheckDigit ()
-        {
-            string input = Console.ReadLine();
-        }
 
         private static void BasicProgramInfo ()
         {
@@ -166,7 +228,7 @@ namespace Budget
             Console.WriteLine("Orlando Valdez");
         }
 
-        private static void DisplayError ( string message)
+        private static void DisplayError ( string message )
         {
             Console.WriteLine(message);
         }
