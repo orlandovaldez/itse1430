cdd01cb99f58d5e6edfc7f2638867fe22035869d
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-02-22 02:40:10 -0600

    Story 1 Setup

---
+++ labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Budget
+ * ITSE 1430
+ * Semester 2021
+ * Orlando Valdez
+ */
+
+using System;
+
+namespace CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
