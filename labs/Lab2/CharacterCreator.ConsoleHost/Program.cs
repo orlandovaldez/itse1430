7d298527d62be00cd41a877a80e2b3f1249e3fc8
Orlando Valdez <orlando.valdez@my.tccd.edu>
2021-02-28 00:47:17 -0600

    Added Quiting The Program

--- labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/CharacterCreator.ConsoleHost/Program.cs
@@ -14,6 +14,64 @@ namespace CharacterCreator.ConsoleHost
         static void Main(string[] args)
         {
             DisplayProgramInfo();
+
+            bool done = false;
+            do
+            {
+                char mainMenuOption = DisplayMainMenu();
+
+                switch (DisplayMainMenu())
+                {
+                    case 'A':
+                    break;
+                    case 'B':
+                    break;
+                    case 'C':
+                    break;
+                    case 'Q': done = QuitProgram();
+                    break;
+
+                    default: DisplayError("Invalid Selection, Try Again.");
+                    break;
+                };
+            }while (!done);
+        }
+
+        static bool QuitProgram ()
+        {
+            if (ReadBoolean("Are you sure you want to quit (Y/N)? "))
+                return true;
+            return false;
+        }
+
+        private static void DisplayError ( string message )
+        {
+            Console.WriteLine(message);
+        }
+
+        static char DisplayMainMenu ()
+        {
+            do
+            {
+                Console.WriteLine("A) PlaceHolder ");
+                Console.WriteLine("B) PlaceHolder ");
+                Console.WriteLine("Q) Quit Program ");
+
+                string menuInput = Console.ReadLine();
+
+                switch (menuInput)
+                {
+                    case "A":
+                    case "a": return 'A';
+
+                    case "B":
+                    case "b": return 'B';
+
+                    case "Q":
+                    case "q": return 'Q';
+                };
+
+            } while (true);
         }
 
         static void DisplayProgramInfo () //Displays Basic Program Information 
@@ -23,11 +81,20 @@ namespace CharacterCreator.ConsoleHost
             Console.WriteLine();
         }
 
-        static void DisplayMenuOption ()
+        static bool ReadBoolean ( string message )
         {
-            Console.WriteLine();
+            Console.Write(message);
 
-            
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.Y: return true;
+                    case ConsoleKey.N: return false;
+                };
+            } while (true);
         }
     }
 }
